eb9cf130579b8446771bf0ee72eb961f541488dd
xiuno <axiuno@gmail.com>
2013-05-05 19:31:13 +0800

    new file:   plugin/xn_friendlink/bbsadmin/friendlink_control.class.php 	new file:   plugin/xn_friendlink/bbsadmin/friendlink_list.htm 	new file:   plugin/xn_friendlink/bbsadmin/friendlink_update.htm 	new file:   plugin/xn_friendlink/conf.php 	new file:   plugin/xn_friendlink/friendlink.class.php 	new file:   plugin/xn_friendlink/index_bbs_after.php 	new file:   plugin/xn_friendlink/index_index_stat_after.htm 	new file:   plugin/xn_friendlink/install.php 	new file:   plugin/xn_friendlink/setting.php 	modified:   view/index.htm

---
+++ plugin/xn_friendlink/conf.php
@@ -0,0 +1,9 @@
+<?php
+
+return 	array (
+	'name' => '友情链接',		// 插件名
+	'brief' => '友情链接。',
+	'version' => '1.0.0',		// 插件版本
+	'bbs_version' => '2.0.2',		// 插件支持的 Xiuno BBS 版本
+);
+?>
---
+++ plugin/xn_friendlink/install.php
@@ -0,0 +1,26 @@
+<?php
+
+!defined('FRAMEWORK_PATH') && exit('FRAMEWORK_PATH not defined.');
+
+// 改文件会被 include 执行。
+if($this->conf['db']['type'] != 'mongodb') {
+	$db = $this->user->db;	// 与 user model 同一台 db
+	$tablepre = $db->tablepre;
+	try {
+	$db->query("CREATE TABLE bbs_friendlink(
+  linkid int(10) unsigned NOT NULL auto_increment ,
+  type tinyint(1) NOT NULL default '0',	
+  rank tinyint(1) unsigned NOT NULL default '0',
+  sitename char(16) NOT NULL default '',
+  url char(64) NOT NULL default '',
+  logo char(64) NOT NULL default '',
+  PRIMARY KEY (linkid),
+  KEY type (type, rank)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;");
+	} catch (Exception $e) {}
+}
+
+$dir1 = $this->conf['upload_path'].'friendlink/';
+!is_dir($dir1) && mkdir($dir1, 0777);
+
+?>
