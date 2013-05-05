c68c4af0e1c09cf5fa0d8c023ecc3127ba1b19f2
xiuno <axiuno@gmail.com>
2013-05-05 22:48:15 +0800

    modified:   upgrade/xn202_to_xn203.php

--- upgrade/xn202_to_xn203.php
+++ upgrade/xn202_to_xn203.php
@@ -36,21 +36,15 @@ if(IN_SAE) {
 core::init();
 core::ob_start();
 $step = core::gpc('step');
-empty($step) && $step = 'alter_table';
+empty($step) && $step = 'complete';
 $start = intval(core::gpc('start'));
 
 // 升级配置文件
-if($step == 'alter_table') {
-	alter_table();
-} elseif($step == 'upgrade_attach_download') {
-	upgrade_attach_download();
-} elseif($step == 'upgrade_digest') {
-	upgrade_digest();
-} elseif($step == 'complete') {
+if($step == 'complete') {
 	complete();
 }
 
-function alter_table() {
+function complete() {
 	global $conf;
 	
 	// 修改配置文件
@@ -64,154 +58,10 @@ function alter_table() {
 		file_put_contents($conffile, $s);
 	}
 	
-	// 2. 修改表结构
-	$sql = "
-alter table bbs_attach_download add column fid int(10) unsigned NOT NULL default '0';
-alter table bbs_attach_download drop key aid;
-alter table bbs_attach_download add key fidaid(fid,aid);
-alter table bbs_thread drop key tid;
-alter table bbs_thread drop key fid_2;
-CREATE TABLE IF NOT EXISTS bbs_thread_new (
-  fid smallint(6) NOT NULL default '0',			# 版块id
-  tid int(11) unsigned NOT NULL default '0',		# 主题id
-  dateline int(10) unsigned NOT NULL default '0',	# 发帖时间
-  lastpost int(10) unsigned NOT NULL default '0',	# 最后回复时间
-  PRIMARY KEY (tid),					# 
-  UNIQUE KEY (fid, tid),				# 
-  KEY (lastpost)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-CREATE TABLE IF NOT EXISTS bbs_thread_digest (
-  fid smallint(6) NOT NULL default '0',			# 版块id
-  tid int(11) unsigned NOT NULL default '0',		# 主题id
-  digest tinyint(3) unsigned NOT NULL default '0',	# 精华等级
-  PRIMARY KEY (tid),					# 
-  UNIQUE KEY (fid, tid)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-DROP TABLE IF EXISTS bbs_banip;
-CREATE TABLE bbs_banip (
-  banid bigint(11) unsigned NOT NULL auto_increment,	# banid
-  ip0 smallint(11) NOT NULL default '0',		# 
-  ip1 smallint(11) NOT NULL default '0',		# 
-  ip2 smallint(11) NOT NULL default '0',		# 
-  ip3 smallint(11) NOT NULL default '0',		# 
-  uid int(11) unsigned NOT NULL default '0',		# 添加人
-  dateline int(11) unsigned NOT NULL default '0',	# 添加时间
-  expiry int(11) unsigned NOT NULL default '0',		# 过期时间
-  PRIMARY KEY (banid),
-  KEY (ip0, ip1, ip2, ip3)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-UPDATE bbs_thread_type SET rank=0;
-";
-
-	$db = new db_mysql($conf['db']['mysql']);
-	$s = $sql;
-	$s = str_replace("\r\n", "\n", $s);
-	$s = preg_replace('#\n\#[^\n]*?\n#is', "\n", $s);	// 去掉注释行
-	$s = preg_replace('#;\s+#is', ";\n", $s);
-	$sqlarr = explode(";\n", $s);
-	$tablepre = $db->tablepre;
-	
-	foreach($sqlarr as $sql) {
-		if(trim($sql)) {
-			$sql = str_replace('bbs_', $tablepre, $sql);
-			try {
-				$db->query($sql);
-			} catch (Exception $e) {
-				$error = $e->getMessage();
-				// Duplicate column, Table exists, column/key does not exists
-				if(mysql_errno() != 1060 && mysql_errno() != 1050 && mysql_errno() != 1091) {
-					//echo mysql_errno();
-					//message($error);
-					//break;
-				}
-				echo "<p>此错误可以忽略：".$error."</p>";
-			}
-		}
-	}
-	
-	$db->truncate('runtime');
-	
-	if($conf['version'] == '2.0.0') {
-		message('升级表结构完成，接下来升级 upgrade_attach_download...', '?step=upgrade_attach_download');
-	} else {
-		message('升级即将完成', '?step=upgrade_digest');
-	}
-}
-
-// 典型的跳转框架，修正 fid
-function upgrade_attach_download() {
-	global $start;
-	global $conf;
-	$db = new db_mysql($conf['db']['mysql']);
-	$count = core::gpc('count');
-	if(empty($count)) {
-		$count = $db->index_count('attach_download');
-	}
-	$mattachdown = new attach_download($conf);
-	$mattach = new attach($conf);
-	if($start < $count) {
-		$limit = DEBUG ? 20 : 2000;
-		$arrlist = $mattachdown->index_fetch(array(), array(), $start, $limit);
-		foreach($arrlist as $attachdown) {
-			$attachlist = $mattach->index_fetch(array('aid'=>$attachdown['aid']), array(), 0, 1);
-			$attach = empty($attachlist) ? array() : array_pop($attachlist);
-			if(empty($attach)) continue;
-			$attachdown['fid'] = $attach['fid'];
-			$db->update("attach_download-uid-$attach[uid]-fid-0-aid-$attach[aid]-", $attachdown);
-		}
-		$start += $limit;
-		message("正在升级 upgrade_attach_download, 一共: $count, 当前: $start...", "?step=upgrade_attach_download&start=$start&count=$count", 0);
-	} else {
-		message('升级 upgrade_attach_download 完成', '?step=complete');
-	}
-}
-
-
-// 典型的跳转框架
-function upgrade_digest() {
-	global $start;
-	global $conf;
-	$db = new db_mysql($conf['db']['mysql']);
-	$count = core::gpc('count');
-	if(empty($count)) {
-		$count = $db->index_count('thread');
-	}
-	$mthread = new thread($conf);
-	$mdigest = new thread_digest($conf);
-	if($start < $count) {
-		$limit = DEBUG ? 20 : 2000;
-		$arrlist = $mthread->index_fetch(array(), array(), $start, $limit);
-		foreach($arrlist as $arr) {
-			if(empty($arr['digest'])) continue;
-			$mdigest->create(array('fid'=>$arr['fid'], 'tid'=>$arr['tid'], 'digest'=>$arr['digest']));
-		}
-		$start += $limit;
-		message("正在升级 upgrade_digest, 一共: $count, 当前: $start...", "?step=upgrade_digest&start=$start&count=$count", 0);
-	} else {
-		message('升级 upgrade_digest 完成', '?step=complete');
-	}
-}
-
-
-function complete() {
-	global $conf;
-	$mthread = new thread($conf);
-	$mthreadnew = core::model($conf, 'thread_new');
-	$newlist = $mthread->index_fetch(array('lastpost' => array('>'=>$_SERVER['time'] - 86400 * 30)), array(), 0, 10000);
-	foreach($newlist as $new) {
-		$mthreadnew->create(array('fid'=>$new['fid'], 'tid'=>$new['tid'], 'lastpost'=>$new['lastpost']));
-	}
-	unset($newlist);
-	
-	$runtime = new runtime($conf);
-	$kv = new kv($conf);
-	$runtime->xset('view_path', array());
-	$kv->xset('view_path', array());
-	$kv->save_changed();
-	$runtime->save_changed();
+	$mconf = new xn_conf($configfile);
+	$mconf->set('plugin_on', 1);
+	$mconf->set('plugin_disable', 0);
+	$mconf->save();
 	
 	message('升级完毕。', './');
 }
