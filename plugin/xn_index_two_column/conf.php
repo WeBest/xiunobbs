62bc1c1b18923b035cf236bf47efe758e4f3bd16
axiuno <axiuno@gmail.com>
2013-05-06 17:52:30 +0800

    modified:   plugin/xn_index_two_column/conf.php 	renamed:    plugin/menu_forum_list/conf.php -> plugin/xn_menu_forum_list/conf.php 	renamed:    plugin/menu_forum_list/footer_end.htm -> plugin/xn_menu_forum_list/footer_end.htm 	renamed:    plugin/menu_forum_list/header_menu_end.htm -> plugin/xn_menu_forum_list/header_menu_end.htm 	renamed:    plugin/menu_forum_list/index_control_after.php -> plugin/xn_menu_forum_list/index_control_after.php 	renamed:    plugin/menu_forum_list/plugin_menu_forum_list.htm -> plugin/xn_menu_forum_list/plugin_menu_forum_list.htm

--- plugin/xn_index_two_column/conf.php
+++ plugin/xn_index_two_column/conf.php
@@ -5,6 +5,6 @@ return 	array (
 	'brief' => '最新主题 + 精华主题。',
 	'version' => '1.0.0',			// 插件版本
 	'bbs_version' => '2.0.3',		// 插件支持的 Xiuno BBS 版本
-	'rank' => 98,							
+	'rank' => 100,							
 );
 ?>
