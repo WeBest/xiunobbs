0ef2ace251e9f01acb598d9700727cca7db4fe1c
xiuno <axiuno@gmail.com>
2013-05-07 22:24:39 +0800

    modified:   upgrade/xn202_to_xn203.php

--- upgrade/xn202_to_xn203.php
+++ upgrade/xn202_to_xn203.php
@@ -4,14 +4,15 @@
  * Copyright (C) xiuno.com
  */
 
-// 本程序用来升级测试版本的 Xiuno BBS 2.0.* 到 Xiuno BBS 2.0.2
+// 本程序用来升级测试版本的 Xiuno BBS 2.0.2 到 Xiuno BBS 2.0.3
 /*
 	流程：
-		1. 覆盖 control, model, view, admin, xiunophp 这五个目录
-		2. 将 upgrade/xn201_to_xn202.php 放置于根目录
-		3. 访问 http://www.domain.com/xn201_to_xn202.php
+		1. 下载 http://www.xiuno.com/down/xiuno_bbs_2.0.3.patch.tar.gz 
+		2. 解压覆盖到网站根目录
+		3. 将 upgrade/xn202_to_xn203.php 放置于根目录
+		3. 访问 http://www.domain.com/xn202_to_xn203.php
 		4. 清空 tmp
-		5. 删除文件 xn201_to_xn202.php
+		5. 删除文件 xn202_to_xn203.php
 */
 
 @set_time_limit(0);
@@ -54,9 +55,9 @@ function complete() {
 		$s = str_replace("'thread_views'=>array('thread_views', 'tid', 'tid')", "'thread_views'=>array('thread_views', 'tid', 'tid'),\n\t\t'thread_new'=>array('thread_new', 'tid')", $s);
 		$s = str_replace("'version' => '2.0.0 Release'", "'version' => '2.0.2'", $s);
 		$s = str_replace("'version' => '2.0.1'", "'version' => '2.0.2'", $s);
-		$s = str_replace("'version' => '2.0.2'", "'version' => '2.0.3'", $s);
-		file_put_contents($conffile, $s);
 	}
+	$s = str_replace("'version' => '2.0.2'", "'version' => '2.0.3'", $s);
+	file_put_contents($conffile, $s);
 	
 	$mconf = new xn_conf($configfile);
 	$mconf->set('plugin_on', 1);
@@ -80,7 +81,7 @@ function complete() {
 	if(isset($arr['rename_username'])) unset($arr['rename_username']);
 	file_put_contents($settingfile, core::json_encode($arr));
 	
-	message('升级完毕。', './');
+	message('老插件已经清除，请通过后台在线插件安装新插件，升级完毕。', './');
 }
 
 function message($s, $url = '', $timeout = 2) {
@@ -100,13 +101,13 @@ function message($s, $url = '', $timeout = 2) {
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>Xiuno BBS 2.0.* - Xiuno BBS 2.0.2 升级程序 </title>
+		<title>Xiuno BBS 2.0.2 - Xiuno BBS 2.0.3 升级程序 </title>
 		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
 		<link rel="stylesheet" type="text/css" href="view/common.css" />
 	</head>
 	<body>
 	<div id="header" style="overflow: hidden;">
-		<h3 style="color: #FFFFFF;line-height: 26px;margin-left: 16px;">Xiuno BBS 2.0.* - Xiuno BBS 2.0.2  升级程序</h3>
+		<h3 style="color: #FFFFFF;line-height: 26px;margin-left: 16px;">Xiuno BBS 2.0.2 - Xiuno BBS 2.0.3  升级程序</h3>
 		<p style="color: #BBBBBB;margin-left: 16px;">本程序用来升级Xiuno BBS 2.0.1。</p>
 	</div>
 	<div id="body" style="padding: 16px;">
