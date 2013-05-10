319bca28827f2afdcefd99dbfcb22e7c0af050f2
axiuno <axiuno@gmail.com>
2013-05-10 12:04:50 +0800

    modified:   plugin/xn_syntaxlighter/conf.php 	modified:   plugin/xn_syntaxlighter/thread_index_end.htm

--- plugin/xn_syntaxlighter/conf.php
+++ plugin/xn_syntaxlighter/conf.php
@@ -3,8 +3,8 @@
 return 	array (
 	'name' => '语法高亮',
 	'brief' => '支持 HTML JS CSS PHP C JS C++ 等常用语法的高亮。',
-	'version' => '1.0.0',
-	'bbs_version'=>'2.*',
+	'version' => '1.0.1',
+	'bbs_version'=>'2.0.3',
 	'cateid' => 2,
 	'pluginid' => 67,
 );
