9bde271f274c8d66025ea22ce29d63b326434222
axiuno <axiuno@gmail.com>
2013-05-16 10:51:24 +0800

    modified:   conf.php

--- plugin/xn_friendlink/conf.php
+++ plugin/xn_friendlink/conf.php
@@ -3,7 +3,7 @@
 return 	array (
 	'name' => '友情链接',		// 插件名
 	'brief' => '友情链接。',
-	'version' => '1.0.2',		// 插件版本
+	'version' => '1.0.3',		// 插件版本
 	'bbs_version' => '2.0.2',		// 插件支持的 Xiuno BBS 版本
 	'rank' => 99,	
 	'cateid' => 2,
