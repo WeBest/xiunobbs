159e69ad7a31cf13a3ed3a36d6a36a1d5642f014
axiuno <axiuno@gmail.com>
2013-05-16 10:30:42 +0800

    modified:   xn_friendlink/index_index_before.php

--- plugin/xn_friendlink/index_index_before.php
+++ plugin/xn_friendlink/index_index_before.php
@@ -1,11 +1,11 @@
 
 		$friendlinklist = array();
-		$friendlinklist[0] = $this->friendlink->index_fetch(array('type'=>0), array('rank'=>-1), 0, 1000);
+		$friendlinklist[0] = $this->friendlink->index_fetch(array('type'=>0), array('rank'=>1), 0, 1000);
 		foreach($friendlinklist[0] as &$friendlink) {
 			$this->friendlink->format($friendlink);
 		}
 		
-		$friendlinklist[1] = $this->friendlink->index_fetch(array('type'=>1), array('rank'=>-1), 0, 1000);
+		$friendlinklist[1] = $this->friendlink->index_fetch(array('type'=>1), array('rank'=>1), 0, 1000);
 		foreach($friendlinklist[1] as &$friendlink) {
 			$this->friendlink->format($friendlink);
 		}
