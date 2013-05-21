66f25ef5c969ce51ab144d6864b90e2b4dedd0ca
axiuno <axiuno@gmail.com>
2013-05-21 18:14:24 +0800

    modified:   plugin/xn_clear_rubbish/setting.php 	modified:   plugin/xn_clear_rubbish/xn_clear_rubbish.htm

--- plugin/xn_clear_rubbish/setting.php
+++ plugin/xn_clear_rubbish/setting.php
@@ -3,37 +3,25 @@
 $error = $input = array();
 
 $start = intval(core::gpc('start'));
+$fid = intval(core::gpc('fid'));
 
 if(!$this->form_submit() && empty($start)) {
+	
+	$forumoptions = $this->forum->get_options($this->_user['uid'], $this->_user['groupid'], $fid, $defaultfid);
+	$this->view->assign('forumoptions', $forumoptions);
+	
 	$this->view->assign('dir', $dir);
 	$this->view->display('xn_clear_rubbish.htm');
 } else {
-	
-	$count_user = core::gpc('count_user', 'R');
-	$count_forum = core::gpc('count_forum', 'R');
-	$count_threadtype = core::gpc('count_threadtype', 'R');
 	
 	if(empty($start)) {
 		// 锁住
 		$this->runtime->xset('site_runlevel', 4, 'runtime');
 		$this->kv->xset('site_runlevel', 4, 'conf');
-		// 创建索引
-		try {$this->thread->index_create(array('tid'=>1)); } catch (Exception $e) {}
-		if($count_user) {
-			$this->user->index_update(array(), array('digests'=>0, 'threads'=>0));
-		}
-		if($count_forum) {
-			$this->forum->index_update(array(), array('digests'=>0, 'threads'=>0));
-		}
-		if($count_threadtype) {
-			$this->thread_type_count->truncate();
-			$this->thread_type_data->truncate();
-		}
-		$this->thread_digest->truncate();
 	}
 	
 	$limit = DEBUG ? 20 : 200;
-	$arrlist = $this->thread->index_fetch(array('tid'=>array('>'=>$start)), array('tid'=>1), 0, $limit);
+	$arrlist = $this->thread->index_fetch(array('fid'=>$fid, 'tid'=>array('>'=>$start)), array('tid'=>1), 0, $limit);
 	if(!empty($arrlist)) {
 		$user_digests = $user_threads = $forum_threads = $forum_digests = array();
 		foreach($arrlist as $arr) {
@@ -60,34 +48,6 @@ if(!$this->form_submit() && empty($start)) {
 				if($arr['typeid1'] || $arr['typeid2'] || $arr['typeid3'] || $arr['typeid4']) {
 					$this->thread_type_data->xcreate($arr['fid'], $arr['tid'], $arr['typeid1'], $arr['typeid2'], $arr['typeid3'], $arr['typeid4']);
 				}
-			}
-		}
-		if($user_threads) {
-			foreach($user_threads as $_uid=>$_count) {
-				$user = $this->user->read($_uid);
-				$user['threads'] += $_count;
-				$this->user->update($user);
-			}
-		}
-		if($user_digests) {
-			foreach($user_digests as $_uid=>$_count) {
-				$user = $this->user->read($_uid);
-				$user['digests'] += $_count;
-				$this->user->update($user);
-			}
-		}
-		if($forum_threads) {
-			foreach($forum_threads as $_fid=>$_count) {
-				$forum = $this->forum->read($_fid);
-				$forum['threads'] += $_count;
-				$this->forum->update($forum);
-			}
-		}
-		if($forum_digests) {
-			foreach($forum_digests as $_fid=>$_count) {
-				$forum = $this->forum->read($_fid);
-				$forum['digests'] += $_count;
-				$this->forum->update($forum);
 			}
 		}
 		$start = $arr['tid'];
