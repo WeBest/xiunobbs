a4c0268ea2dc44f469b067e8e2ef5f5bca46087c
xiuno <axiuno@gmail.com>
2013-06-12 15:56:06 +0800

    modified:   plugin/xn_mod_lock/common_control_check_access_adminaction_after.php 	new file:   plugin/xn_mod_lock/conf.php

--- plugin/xn_mod_lock/common_control_check_access_adminaction_after.php
+++ plugin/xn_mod_lock/common_control_check_access_adminaction_after.php
@@ -1,2 +1,2 @@
-$adminaction = array_push(array('allowupdown'=>'提升主题'));
-$adminaction = array_push(array('allowlock'=>'锁住主题'));
+$adminaction['allowupdown'] = '提升主题';
+$adminaction['allowlock'] = '锁定主题';
