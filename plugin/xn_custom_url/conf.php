4e59be9dba3e8192ead47e6e011b4007d9b19b1f
axiuno <axiuno@gmail.com>
2013-06-28 15:11:52 +0800

    modified:   plugin/xn_custom_url/conf.php 	modified:   view/thread_list_line.inc.htm

--- plugin/xn_custom_url/conf.php
+++ plugin/xn_custom_url/conf.php
@@ -3,7 +3,7 @@
 return 	array (
 	'name' => '自定义URL',		// 插件名
 	'brief' => '允许版主自定义URL，增强SEO，需要自己配置 URL-Rewrite 规则，方法请参考：http://bbs.xiuno.com/thread-index-fid-35-tid-2877.htm',
-	'version' => '1.0.2',		// 插件版本
+	'version' => '1.0.3',		// 插件版本
 	'bbs_version' => '2.0.3',		// 插件支持的 Xiuno BBS 版本
 );
 ?>
