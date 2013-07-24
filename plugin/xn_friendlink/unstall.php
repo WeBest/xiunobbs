df6c09c31e428aeb9ddd56a3cc6ecc02dc1dea80
axiuno <axiuno@gmail.com>
2013-07-24 15:01:08 +0800

    new file:   plugin/xn_friendlink/unstall.php

---
+++ plugin/xn_friendlink/unstall.php
@@ -0,0 +1,18 @@
+<?php
+
+!defined('FRAMEWORK_PATH') && exit('FRAMEWORK_PATH not defined.');
+
+if(DEBUG) {
+
+	// 改文件会被 include 执行。
+	if($this->conf['db']['type'] != 'mongodb') {
+		$db = $this->user->db;	// 与 user model 同一台 db
+		$tablepre = $db->tablepre;
+		try {$db->query("DROP TABLE IF EXISTS {$tablepre}friendlink;");} catch (Exception $e) {}
+	}
+	
+	$dir1 = $this->conf['upload_path'].'friendlink/';
+	!is_dir($dir1) && misc::rmdir($dir1);
+
+}
+?>
