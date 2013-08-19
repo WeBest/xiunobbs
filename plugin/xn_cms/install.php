9e1b59b098dff4755841cdf3e1311fbfdf86b411
xiuno <axiuno@gmail.com>
2013-08-19 13:52:16 +0800

    modified:   plugin/xn_cms/install.php 	modified:   plugin/xn_shop/install.php 	modified:   view/pm_list_ajax.htm

--- plugin/xn_cms/install.php
+++ plugin/xn_cms/install.php
@@ -3,48 +3,44 @@
 !defined('FRAMEWORK_PATH') && exit('FRAMEWORK_PATH not defined.');
 
 // 改文件会被 include 执行。
-if($this->conf['db']['type'] != 'mongodb') {
-	$db = $this->user->db;	// 与 user model 同一台 db
-	$db->table_drop('cms_channel');
-	$db->table_create('cms_channel', array(
-		array('channelid', 'int(11)'), 
-		array('rank', 'int(11)'), 
-		array('layout', 'int(11)'), 
-		array('name', 'char(32)'), 
-	));
-	$db->index_create('cms_channel', array('channelid'=>1));
-	//$db->maxid('cms_article-articleid', 100);
-	//$cms_channel = core::model($this->conf, 'cms_channel', array('channelid'), 'channelid');
-	//$cms_channel->create();
-	//$db->set("cms_channel-channelid-1", array(''));
-	
-	$db->table_drop('cms_cate');
-	$db->table_create('cms_cate', array(
-		array('channelid', 'int(11)'), 
-		array('cateid', 'int(11)'), 
-		array('name', 'char(32)'),
-		array('rank', 'int(11)'), 
-		array('articles', 'int(11)'), 
-	));
-	$db->index_create('cms_cate', array('channelid'=>1, 'cateid'=>1));
-	
-	$db->table_drop('cms_article');
-	$db->table_create('cms_article', array(
-		array('channelid', 'int(11)'), 
-		array('cateid', 'int(11)'), 
-		array('articleid', 'int(11)'), 
-		array('subject', 'varchar(255)'), 
-		array('message', 'longtext'), 
-		array('username', 'char(16)'), 
-		array('dateline', 'int(11)'), 
-		array('rank', 'int(11)'), 
-		array('views', 'int(11)'),
-	));
-	$db->index_create('cms_article', array('channelid'=>1, 'cateid'=>1, 'rank'=>1));
-	$db->index_create('cms_article', array('articleid'=>1));
-	
-	// 预留100篇文章
-	$db->maxid('cms_article-articleid', 100);
-}
+
+$db = $this->user->db;	// 与 user model 同一台 db
+
+$db->table_drop('cms_channel');
+$db->table_create('cms_channel', array(
+	array('channelid', 'int(11)'), 
+	array('rank', 'int(11)'), 
+	array('layout', 'int(11)'), 
+	array('name', 'char(32)'), 
+));
+$db->index_create('cms_channel', array('channelid'=>1));
+
+$db->table_drop('cms_cate');
+$db->table_create('cms_cate', array(
+	array('channelid', 'int(11)'), 
+	array('cateid', 'int(11)'), 
+	array('name', 'char(32)'),
+	array('rank', 'int(11)'), 
+	array('articles', 'int(11)'), 
+));
+$db->index_create('cms_cate', array('channelid'=>1, 'cateid'=>1));
+
+$db->table_drop('cms_article');
+$db->table_create('cms_article', array(
+	array('channelid', 'int(11)'), 
+	array('cateid', 'int(11)'), 
+	array('articleid', 'int(11)'), 
+	array('subject', 'varchar(255)'), 
+	array('message', 'longtext'), 
+	array('username', 'char(16)'), 
+	array('dateline', 'int(11)'), 
+	array('rank', 'int(11)'), 
+	array('views', 'int(11)'),
+));
+$db->index_create('cms_article', array('channelid'=>1, 'cateid'=>1, 'rank'=>1));
+$db->index_create('cms_article', array('articleid'=>1));
+
+// 预留400篇文章, 20 * 20
+$db->maxid('cms_article-articleid', 400);
 
 ?>
--- plugin/xn_shop/install.php
+++ plugin/xn_shop/install.php
@@ -2,42 +2,60 @@
 
 !defined('FRAMEWORK_PATH') && exit('FRAMEWORK_PATH not defined.');
 
-// 改文件会被 include 执行。
-if($this->conf['db']['type'] != 'mongodb') {
-	$db = $this->user->db;	// 与 user model 同一台 db
-	$tablepre = $db->tablepre;
-	
-	// 商品
-	$db->query("CREATE TABLE IF NOT EXISTS {$tablepre}shop_goods (
-		  goodsid int(11) unsigned NOT NULL auto_increment,
-		  name char(32) NOT NULL DEFAULT '',
-		  price int(11) unsigned NOT NULL DEFAULT '0',
-		  dateline int(11) unsigned NOT NULL DEFAULT '0',  #  上架时间
-		  PRIMARY KEY(goodsid)
-	);");
-	
-	// 订单
-	$db->query("CREATE TABLE IF NOT EXISTS {$tablepre}shop_order (
-		  orderid int(11) unsigned NOT NULL DEFAULT '0',
-		  goodsid int(11) unsigned NOT NULL DEFAULT '0',
-		  uid int(11) unsigned NOT NULL DEFAULT '0',
-		  username char(16) NOT NULL DEFAULT '',
-		  rank int(11) unsigned NOT NULL DEFAULT '0',
-		  PRIMARY KEY(orderid)
-	);");
-	
-	// 评论
-	$db->query("CREATE TABLE IF NOT EXISTS {$tablepre}shop_reply (
-		  goodsid int(11) unsigned NOT NULL DEFAULT '0',
-		  replyid int(11) unsigned NOT NULL DEFAULT '0',
-		  message longtext NOT NULL default '',
-		  uid int(11) unsigned NOT NULL DEFAULT '0',
-		  username char(16) NOT NULL DEFAULT '',
-		  dateline int(11) unsigned NOT NULL DEFAULT '0',
-		  rank int(11) unsigned NOT NULL DEFAULT '0',
-		  PRIMARY KEY(replyid)
-	);");
-	
-}
+$db = $this->user->db;	// 与 user model 同一台 db
 
+$db->table_drop('shop_cate');
+$db->table_create('shop_cate', array(
+	array('cateid', 'int(11)'), 
+	array('name', 'int(11)'), 
+	array('goods', 'int(11)'), 
+	array('rank', 'int(11)'), 
+));
+$db->index_create('shop_cate', array('cateid'=>1, 'rank'=>1));
+
+$db->table_drop('shop_good');
+$db->table_create('shop_good', array(
+	array('goodid', 'int(11)'), 
+	array('name', 'int(11)'), 
+	array('price', 'int(11)'), 
+	array('dateline', 'int(11)'), 
+	array('orders', 'int(11)'), 
+	array('replies', 'int(11)'), 
+));
+$db->index_create('shop_goods', array('goodsid'=>1));
+
+$db->table_drop('shop_order');
+$db->table_create('shop_order', array(
+	array('orderid', 'int(11)'), 
+	array('goodid', 'int(11)'), 
+	array('uid', 'int(11)'), 
+	array('dateline', 'int(11)'), 
+	array('year', 'int(11)'), 
+	array('month', 'int(11)'), 
+	array('day', 'int(11)'), 
+	array('status', 'int(11)'),  // "0":"等待支付", "1":"已支付，等待发货", "2":"已发货，等待收货", "3":"已收货，交易完毕", "4":"无效订单"
+	
+	array('alipay_email', 'char(60)'), 
+	array('alipay_orderid', 'char(60)'), 
+	array('alipay_fee', 'int(11)'), 
+	array('alipay_receive_name', 'char(10)'), 
+	array('alipay_receive_phone', 'char(20)'), 
+	array('alipay_receive_mobile', 'char(10)'), 
+));
+$db->index_create('shop_order', array('orderid'=>1));
+$db->index_create('shop_order', array('year'=>1, 'month'=>1, 'day'=>1));
+
+$db->table_drop('shop_reply');
+$db->table_create('shop_reply', array(
+	array('replyid', 'int(11)'), 
+	array('goodid', 'int(11)'), 
+	array('uid', 'int(11)'), 
+	array('dateline', 'int(11)'), 
+	array('message', 'text'), 
+));
+$db->index_create('shop_reply', array('replyid'=>1));
+$db->index_create('shop_reply', array('goodid'=>1));
+
+// 节省一个表，按照 id 存放。
+// 附件存放于 attach_shop，按照天存储， upload/attach_shop/000/000/1_1234.jpg, upload/attach_shop/000/000/1_1235.jpg, 
 ?>
